d401033b728d40e6d18a4a17307a892b8e969337
glov1c <karasev_es@mail.ru>
2025-12-13 16:43:41 -0800

    added .gitignore

--- main.c
+++ main.c
@@ -5,6 +5,22 @@
 #include "operations.h"
 
 int main(int argc, char** argv) {
+    if (argc != 3) {
+        perror("not correct arguments\n");
+        return EXIT_FAILURE;
+    }
+    FILE* file_ptr = fopen(argv[1], "r");
+    if (file_ptr == NULL) {
+        perror("file error\n");
+        return EXIT_FAILURE;
+    }
     
+    char* buffer = (char*)calloc(128, sizeof(char));
+    if (buffer == NULL) {
+        perror("memory error\n");
+        return EXIT_FAILURE;
+    }
+    
+    fclose(file_ptr);
     return EXIT_SUCCESS;
 }
