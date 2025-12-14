4f03d01e110cc3a86ee0888933ddb61e5edb1621
glov1c <karasev_es@mail.ru>
2025-12-13 16:35:38 -0800

    file decomposition

---
+++ operations.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* replace(char* buffer, char* substr, char* newstr);
+char* deleteStr(char* buffer, char* str, char* newstr);
+char* addToStart(char* buffer, char* newstr);
+char* addToEnd(char* buffer, char* newstr);
---
+++ operations.h
@@ -0,0 +1,6 @@
+#pragma once
+
+char* replace(char* buffer, char* substr, char* newstr);
+char* deleteStr(char* buffer, char* str, char* newstr);
+char* addToStart(char* buffer, char* newstr);
+char* addToEnd(char* buffer, char* newstr);
