6e70226520e8ec757c0521582c888c17100c94d1
glov1c <karasev_es@mail.ru>
2025-12-14 15:48:56 -0800

    replace function

--- main.c
+++ main.c
@@ -10,20 +10,31 @@ int main(int argc, char** argv) {
         perror("not correct arguments\n");
         return EXIT_FAILURE;
     }
-    FILE* file_ptr = fopen(argv[1], "a");
+    
+    FILE* file_ptr = fopen(argv[1], "r");
     if (file_ptr == NULL) {
         perror("file error\n");
         return EXIT_FAILURE;
     }
-    
-    char* buffer = (char*)calloc(128, sizeof(char));
+    //fputs("Hello, world", file_ptr);
+    char* buffer = (char*)calloc(256, sizeof(char));
     if (buffer == NULL) {
         perror("memory error\n");
         return EXIT_FAILURE;
     }
+    char* line = NULL;
+    size_t capacity = 0;
+    while (getline(&line, &capacity, file_ptr) != -1) {
+        strcat(buffer, line);
+    }
+    fputs(buffer, stdout);
+    printf("\n");
+    free(line);
+    fclose(file_ptr);
     
     Command C;
     C = command(argv[2]);
+    printf("%d %s %s\n", C.cnum, C.str1, C.str2);
     switch(C.cnum){
         case 0:
             perror("command error\n");
@@ -36,7 +47,18 @@ int main(int argc, char** argv) {
             addSuffix(buffer, C.str1);
         case 4:
             replace(buffer, C.str1, C.str2);
+//            fputs(buffer, stdout);
+//            printf("\n");
     }
+    
+    file_ptr = fopen(argv[1], "w");
+    if (file_ptr == NULL) {
+        perror("file error\n");
+        return EXIT_FAILURE;
+    }
+    fputs(buffer, file_ptr);
+    printf("\n");
+    free(buffer);
     fclose(file_ptr);
     return EXIT_SUCCESS;
 }
--- operations.c
+++ operations.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 typedef struct {
     int cnum;
@@ -15,19 +16,23 @@ Command command(char* str) {
     int i = 0;
     while (token && i < 3) {
         strsplit[i] = token;
-        printf("%s ", token);
+        printf("%s ", strsplit[i]);
         token = strtok_r(NULL, "/", &saveptr);
+        if (token){
+            i++;
+        }
     }
+    printf("\n");
     if (i == 1 && strcmp(strsplit[1], "d") == 0){
         Command C = {1, strsplit[0], "-"};
         return C;
     }
     if (i == 2) {
-        if (strcmp(strsplit[0], "s") == 0 && strcmp(strsplit[2], "prefix")) {
+        if (strcmp(strsplit[0], "s") == 0 && strcmp(strsplit[2], "prefix") == 0) {
             Command C = {2, strsplit[1], "-"};
             return C;
         }
-        if (strcmp(strsplit[0], "s") == 0 && strcmp(strsplit[2], "suffix")) {
+        if (strcmp(strsplit[0], "s") == 0 && strcmp(strsplit[2], "suffix") == 0) {
             Command C = {3, strsplit[1], "-"};
             return C;
         }
@@ -41,7 +46,49 @@ Command command(char* str) {
     return C;
 }
 
-char* replace(char* buffer, char* substr, char* newstr);
-char* deleteStr(char* buffer, char* str, char* newstr);
-char* addPrefix(char* buffer, char* newstr);
-char* addSuffix(char* buffer, char* newstr);
+char* replace(char* buffer, char* substr, char* newstr){
+    regex_t regex;
+    regmatch_t m;
+    if (regcomp(&regex, substr, REG_EXTENDED)) {
+        perror("regex compilation error");
+        return "";
+    }
+    while(regexec(&regex, buffer, 1, &m, 0) != REG_NOMATCH) {
+        long long start = m.rm_so;
+        long long end = m.rm_eo;
+        //printf("%ld %ld\n", start, end);
+        char* buffer1 = (char*)calloc(256, sizeof(char));
+        if (buffer1 == NULL) {
+            perror("memory error\n");
+            return "";
+        }
+        
+        for(int i = 0; i < strlen(buffer) + strlen(newstr) - (end - start); i++) {
+            if (i < start) {
+                buffer1[i] = buffer[i];
+            }
+            if (i >= start && i < start + strlen(newstr)) {
+                buffer1[i] = newstr[i - start];
+            }
+            if (i >= start + strlen(newstr)) {
+                buffer1[i] = buffer[i + end - start - strlen(newstr)];
+            }
+        }
+        strcpy(buffer, buffer1);
+        //fputs(buffer, stdout);
+        //printf("\n");
+        free(buffer1);
+    }
+    //fputs(buffer, stdout);
+    regfree(&regex);
+    return buffer;
+}
+char* deleteStr(char* buffer, char* str, char* newstr){
+    return buffer;
+}
+char* addPrefix(char* buffer, char* newstr){
+    return buffer;
+}
+char* addSuffix(char* buffer, char* newstr){
+    return buffer;
+}
